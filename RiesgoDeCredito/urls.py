701d128e403588af301a9a7a1e20369d5ad2e2d7
selopez2704 <sebastianlopezmazo@gmail.com>
2022-05-28 01:32:50 -0500

    automatic refresh

--- RiesgoDeCredito/urls.py
+++ RiesgoDeCredito/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from RiesgoCredito import views
+
 urlpatterns = [
     path('',views.rredirect, name='rredirect'),
     path('RiesgoCredito/', include('RiesgoCredito.urls')),
